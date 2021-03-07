b562f0b8ea82cc58c9145aded7cd8374fc402b19
ldylicia <fharrisfoster@gmail.com>
2021-03-06 23:37:31 -0800

    adding updates in js file

--- assets/index.js
+++ assets/index.js
@@ -3,3 +3,12 @@ $(".fas").on("click", function () {
     var text = $(this).parent().prev().val()
     console.log(text)
 })
+
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
+
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
+
+  
+  
