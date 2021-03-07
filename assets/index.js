ca6a01965b98b55304a0e102f829379547a28d25
ldylicia <fharrisfoster@gmail.com>
2021-03-07 00:25:37 -0800

    trying to add localstorage command

--- assets/index.js
+++ assets/index.js
@@ -10,5 +10,38 @@ $("#currentDay").append(currentDay);
 var currentTime = moment().format("LT");
 $("#currentTime").append(currentTime);
 
-  
-  
+
+
+
+
+var tasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
+
+var setTasks = function () {
+    /* add tasks to localStorage */
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var getTasks = function () {
+    /* load the tasks from localStorage and create tasks in the right row */
+
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
+
+        // for each key/value pair in tasks, create a task
+        $.each(tasks, function (hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
+}
